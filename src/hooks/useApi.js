d09784839284414deee3d89fb0775e2a8ff996ed
leon Boakye-Adjei <leon_adjei@hotmail.com>
2021-02-15 12:39:40 +0000

    adding custom hook

---
+++ src/hooks/useApi.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function useApi() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default useApi
